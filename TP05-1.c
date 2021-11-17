fa8e44d73f20aeebbd0d5e3bbc6b540fb1a7cc39
Mehmet Yavuz Katrancı <77459135+myavkat@users.noreply.github.com>
2021-11-17 17:48:51 +0300

    tp05-1

---
+++ TP05-1.c
@@ -0,0 +1,66 @@
+/*
+ * TP05-1.c
+ *
+ *  Created on: Nov 17, 2021
+ *      Author: Mehmet Yavuz Katrancı
+ */
+#include <stdio.h>
+#include<limits.h>
+
+int max(int input1, int input2);
+int min(int input1, int input2);
+
+int main() {
+	int gradeList[8];
+	int maxGrade = INT_MIN;
+	int minGrade = INT_MAX;
+	int sumOfGrades = 0;
+	float gradeAverage;
+	int sizeOfGradeList = (int) sizeof(gradeList) / (int) sizeof(gradeList[0]);
+
+	for (int i = 0; i < sizeOfGradeList; i++) {
+		printf("%d. ogrencinin notunu girin: ", i + 1);
+		scanf("%d", &gradeList[i]);
+		fflush(stdout);
+	}
+
+	printf("\nOgrencilerin notlari:  ");
+	for (int i = 0; i < sizeOfGradeList; i++) {
+		printf("%4d", gradeList[i]);
+	}
+
+	for (int i = 0; i < sizeOfGradeList; i++) {
+		maxGrade = max(maxGrade, gradeList[i]);
+		minGrade = min(minGrade, gradeList[i]);
+		sumOfGrades += gradeList[i];
+	}
+	gradeAverage = (float) sumOfGrades / (float) sizeOfGradeList;
+	printf("\nOrt  : %f\n", gradeAverage);
+	printf("Min  : %d\n", minGrade);
+	printf("Max  : %d\n", maxGrade);
+
+	for(int i=0; i<sizeOfGradeList; i++){
+		if(gradeList[i]==maxGrade){
+			printf("En yuksek notu alan ogrenci no: %d\n", i+1);
+		}
+		else if(gradeList[i]==minGrade){
+			printf("En dusuk notu alan ogrenci no : %d\n", i+1);
+		}
+	}
+
+	return 0;
+}
+
+int max(int input1, int input2) {
+	if (input1 > input2) {
+		return input1;
+	}
+	return input2;
+}
+
+int min(int input1, int input2) {
+	if (input1 < input2) {
+		return input1;
+	}
+	return input2;
+}
