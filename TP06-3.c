dba6c25d2ff08f7d84e7014c551701b085c52ef1
Mehmet Yavuz Katrancı <77459135+myavkat@users.noreply.github.com>
2021-12-01 17:54:08 +0300

    TP06-3.c added
    TP06-2 is not a code task so I skipped adding it to github.

---
+++ TP06-3.c
@@ -0,0 +1,38 @@
+/*
+ * TP06-3.c
+ *
+ *  Created on: Dec 1, 2021
+ *      Author: Mehmet Yavuz Katrancı
+ */
+
+#include<stdio.h>
+
+void swap1(int a, int b);
+void swap2(int *a, int *b);
+int main() {
+	int a, b;
+	printf("Entrez 2 entiers a et b: ");
+	scanf("%d %d", &a, &b);
+	fflush(stdout);
+	printf(" Avant swap : a=%d b=%d\n", a, b);
+	swap1(a, b);
+	printf(" Apres swap1: a=%d b=%d\n", a, b);
+	swap2(&a, &b);
+	printf(" Apres swap2: a=%d b=%d\n", a, b);
+}
+
+void swap1(int a, int b) {
+	int tmp;
+	tmp = a;
+	a = b;
+	b = tmp;
+	return;
+}
+
+void swap2(int *a, int *b) {
+	int tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+	return;
+}
