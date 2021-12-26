b24e2670cc563831cdc0dfd951c72c91042fd619
MyavKat <77459135+myavkat@users.noreply.github.com>
2021-12-26 07:50:29 +0530

    TP09-1.c added

---
+++ TP09/TP09-1.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(int argc, char* argv[]){
+	setvbuf(stdout, NULL, _IONBF, 0); //disables the stdout's buffer
+	
+	if(argc<=1){
+		printf("Yetersiz sayıda arguman");
+		return 1;
+	}
+	FILE* f = fopen(argv[1], "w");
+	if(f==NULL){
+		printf("Pas de fichier!");
+		return 1;
+	}
+	int devam = 1;
+	char nameInput[20];
+	char gradeInput[20];
+	char *p;
+	while(devam){
+		printf("Entrez le prenom: ");
+		fflush(stdout);
+		if(fgets(nameInput, 20, stdin)!=NULL){
+			if( (p = strchr(nameInput, '\n')) != NULL)
+				*p='\0';
+		}
+
+		printf("Entrez la note: ");
+		fflush(stdout);
+		if(fgets(gradeInput, 20, stdin)!=NULL){
+			if( (p = strchr(gradeInput, '\n')) != NULL)
+				*p='\0';
+		}
+
+		fprintf(f, "%s %s\n", nameInput, gradeInput);
+
+		printf("Pour continuer [1], pour terminer [0]: ");
+		fflush(stdout);
+		scanf("%d ", &devam);
+	}
+	printf("Bye bye...\n");
+	return 0;
+}
